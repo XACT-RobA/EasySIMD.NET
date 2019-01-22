f33fbbfcc76350f96510f6e7f303cdcd9c2c9288
Robert Anderson <robertanderson1993@gmail.com>
2019-01-22 22:12:42 +0000

    Added more basic functions for floats

--- EasySIMD.NET.Test/BasicMathsExtensionsTests.cs
+++ EasySIMD.NET.Test/BasicMathsExtensionsTests.cs
@@ -9,7 +9,7 @@ namespace EasySIMD.NET.Test
         private static readonly int precision = 8;
 
         [Fact]
-        public void TestFloatAdd()
+        public void TestAddFloat()
         {
             var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
             var right = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
@@ -17,6 +17,60 @@ namespace EasySIMD.NET.Test
             var expected = new[] { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f };
 
             var actual = left.Add(right);
+
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i], precision);
+            }
+        }
+
+        [Fact]
+        public void TestSubtractFloat()
+        {
+            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+            var right = new[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+
+            var expected = new[] { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f };
+
+            var actual = left.Subtract(right);
+
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i], precision);
+            }
+        }
+
+        [Fact]
+        public void TestMultiplyFloat()
+        {
+            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+            var right = new[] { 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f };
+
+            var expected = new[] { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f };
+
+            var actual = left.Multiply(right);
+
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i], precision);
+            }
+        }
+
+        [Fact]
+        public void TestDivideFloat()
+        {
+            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+            var right = new[] { 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f };
+
+            var expected = new[] { 0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f, 5.0f };
+
+            var actual = left.Divide(right);
 
             Assert.Equal(expected.Length, actual.Length);
 
--- EasySIMD.NET/BasicMathsExtensions.cs
+++ EasySIMD.NET/BasicMathsExtensions.cs
@@ -9,5 +9,20 @@ namespace EasySIMD.NET
         {
             return left.Map(right, (vleft, vright) => vleft + vright, (sleft, sright) => sleft + sright);
         }
+
+        public static float[] Subtract(this float[] left, float[] right)
+        {
+            return left.Map(right, (vleft, vright) => vleft - vright, (sleft, sright) => sleft - sright);
+        }
+
+        public static float[] Multiply(this float[] left, float[] right)
+        {
+            return left.Map(right, (vleft, vright) => vleft * vright, (sleft, sright) => sleft * sright);
+        }
+
+        public static float[] Divide(this float[] left, float[] right)
+        {
+            return left.Map(right, (vleft, vright) => vleft / vright, (sleft, sright) => sleft / sright);
+        }
     }
 }
