0b79933906970ef9c3f7fef588fa479ede1caded
Robert Anderson <robertanderson1993@gmail.com>
2019-01-23 17:37:57 +0000

    Tidied up unit tests

--- EasySIMD.NET.Test/BasicMathsExtensionsFloatingPointTests.cs
+++ EasySIMD.NET.Test/BasicMathsExtensionsFloatingPointTests.cs
@@ -8,17 +8,21 @@ namespace EasySIMD.NET.Test
     {
         private static readonly int precision = 8;
 
+        private static readonly float[] _floatBase = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+        private static readonly float[] _floatOnes = new[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+        private static readonly float[] _floatTwos = new[] { 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f };
+
+        private static readonly double[] _doubleBase = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+        private static readonly double[] _doubleOnes = new[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+        private static readonly double[] _doubleTwos = new[] { 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0 };
+
         // Add
 
         [Fact]
         public void TestAddFloat()
         {
-            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
-            var right = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
-
+            var actual = _floatBase.Add(_floatBase);
             var expected = new[] { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f };
-
-            var actual = left.Add(right);
 
             Assert.Equal(expected.Length, actual.Length);
 
@@ -31,13 +35,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestAddDouble()
         {
-            var left = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-            var right = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-
+            var actual = _doubleBase.Add(_doubleBase);
             var expected = new[] { 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0 };
-
-            var actual = left.Add(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -51,12 +51,8 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestSubtractFloat()
         {
-            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
-            var right = new[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-
+            var actual = _floatBase.Subtract(_floatOnes);
             var expected = new[] { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f };
-
-            var actual = left.Subtract(right);
 
             Assert.Equal(expected.Length, actual.Length);
 
@@ -69,13 +65,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestSubtractDouble()
         {
-            var left = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-            var right = new[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
-
+            var actual = _doubleBase.Subtract(_doubleOnes);
             var expected = new[] { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
-
-            var actual = left.Subtract(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -89,12 +81,8 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestMultiplyFloat()
         {
-            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
-            var right = new[] { 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f };
-
+            var actual = _floatBase.Multiply(_floatTwos);
             var expected = new[] { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f };
-
-            var actual = left.Multiply(right);
 
             Assert.Equal(expected.Length, actual.Length);
 
@@ -107,13 +95,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestMultiplyDouble()
         {
-            var left = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-            var right = new[] { 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0 };
-
+            var actual = _doubleBase.Multiply(_doubleTwos);
             var expected = new[] { 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0 };
-
-            var actual = left.Multiply(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -127,13 +111,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestDivideFloat()
         {
-            var left = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
-            var right = new[] { 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f };
-
+            var actual = _floatBase.Divide(_floatTwos);
             var expected = new[] { 0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f, 5.0f };
-
-            var actual = left.Divide(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -145,13 +125,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestDivideDouble()
         {
-            var left = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
-            var right = new[] { 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0 };
-
+            var actual = _doubleBase.Divide(_doubleTwos);
             var expected = new[] { 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 };
-
-            var actual = left.Divide(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
--- EasySIMD.NET.Test/BasicMathsExtensionsIntegerTests.cs
+++ EasySIMD.NET.Test/BasicMathsExtensionsIntegerTests.cs
@@ -6,18 +6,26 @@ namespace EasySIMD.NET.Test
 {
     public class BasicMathsExtensionsIntegerTests
     {
+        private static readonly short[] _shortBase = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        private static readonly short[] _shortOnes = new short[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        private static readonly short[] _shortTwos = new short[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+
+        private static readonly int[] _intBase = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        private static readonly int[] _intOnes = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        private static readonly int[] _intTwos = new[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+
+        private static readonly long[] _longBase = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
+        private static readonly long[] _longOnes = new[] { 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L };
+        private static readonly long[] _longTwos = new[] { 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L };
+
         // Add
 
         [Fact]
         public void TestAddShort()
         {
-            var left = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            var right = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-
+            var actual = _shortBase.Add(_shortBase);
             var expected = new short[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 };
-
-            var actual = left.Add(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -29,13 +37,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestAddInt()
         {
-            var left = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var right = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
+            var actual = _intBase.Add(_intBase);
             var expected = new[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-
-            var actual = left.Add(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -47,13 +51,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestAddLong()
         {
-            var left = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
-            var right = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
-
+            var actual = _longBase.Add(_longBase);
             var expected = new[] { 2L, 4L, 6L, 8L, 10L, 12L, 14L, 16L, 18L, 20L };
-
-            var actual = left.Add(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -67,13 +67,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestSubtractShort()
         {
-            var left = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            var right = new short[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-
+            var actual = _shortBase.Subtract(_shortOnes);
             var expected = new short[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
-
-            var actual = left.Subtract(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -85,13 +81,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestSubtractInt()
         {
-            var left = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var right = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-
+            var actual = _intBase.Subtract(_intOnes);
             var expected = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            var actual = left.Subtract(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -103,13 +95,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestSubtractLong()
         {
-            var left = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
-            var right = new[] { 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L };
-
+            var actual = _longBase.Subtract(_longOnes);
             var expected = new[] { 0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L };
-
-            var actual = left.Subtract(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -123,13 +111,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestMultiplyShort()
         {
-            var left = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            var right = new short[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
-
+            var actual = _shortBase.Multiply(_shortTwos);
             var expected = new short[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 };
-
-            var actual = left.Multiply(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -141,13 +125,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestMultiplyInt()
         {
-            var left = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var right = new[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
-
+            var actual = _intBase.Multiply(_intTwos);
             var expected = new[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-
-            var actual = left.Multiply(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -159,13 +139,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestMultiplyLong()
         {
-            var left = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
-            var right = new[] { 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L };
-
+            var actual = _longBase.Multiply(_longTwos);
             var expected = new[] { 2L, 4L, 6L, 8L, 10L, 12L, 14L, 16L, 18L, 20L };
-
-            var actual = left.Multiply(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -179,13 +155,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestDivideShort()
         {
-            var left = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            var right = new short[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
-
+            var actual = _shortBase.Divide(_shortTwos);
             var expected = new short[] { 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10 };
-
-            var actual = left.Divide(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -197,13 +169,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestDivideInt()
         {
-            var left = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var right = new[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
-
+            var actual = _intBase.Divide(_intTwos);
             var expected = new[] { 0, 1, 1, 2, 2, 3, 3, 4, 4, 5 };
-
-            var actual = left.Divide(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
@@ -215,13 +183,9 @@ namespace EasySIMD.NET.Test
         [Fact]
         public void TestDivideLong()
         {
-            var left = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
-            var right = new[] { 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L };
-
+            var actual = _longBase.Divide(_longTwos);
             var expected = new[] { 0L, 1L, 1L, 2L, 2L, 3L, 3L, 4L, 4L, 5L };
-
-            var actual = left.Divide(right);
-
+            
             Assert.Equal(expected.Length, actual.Length);
 
             for (int i = 0; i < expected.Length; i++)
