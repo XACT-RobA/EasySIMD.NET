304dfa9dc996351a983206d21d0b380a2c87dbb0
Robert Anderson <robertanderson1993@gmail.com>
2019-01-23 17:53:05 +0000

    Started on statistics, done short sum

---
+++ EasySIMD.NET.Test/StatisticsExtensionsIntegerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using EasySIMD.NET;
+
+namespace EasySIMD.NET.Test
+{
+    public class StatisticsExtensionsIntegerTests
+    {
+        [Fact]
+        public void TestSumShort()
+        {
+            var input = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            var actual = input.Sum();
+            var expected = 210;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ EasySIMD.NET/StatisticsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace EasySIMD.NET
+{
+    public static class StatisticsExtensions
+    {
+        // Sum
+
+        public static short Sum(this short[] input)
+        {
+            var simdLength = CommonSIMD.SimdLength<short>();
+            var simdRange = input.Length - simdLength;
+            var running = new Vector<short>(0);
+
+            int i;
+            for (i = 0; i <= simdRange; i+= simdLength)
+            {
+                var vinput = new Vector<short>(input, i);
+                running += vinput;
+            }
+
+            short output = 0;
+
+            for (int j = 0; j < simdLength; j++)
+            {
+                output += running[j];
+            }
+
+            for (; i < input.Length; i++)
+            {
+                output += input[i];
+            }
+
+            return output;
+        }
+    }
+}
