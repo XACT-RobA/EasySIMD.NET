0952c323a4de3569d926bc48ec50c1505d311909
Robert Anderson <robertanderson1993@gmail.com>
2019-01-24 01:09:51 +0000

    Added methods for floating point sum and product operations

---
+++ EasySIMD.NET.Test/StatisticsExtensionsFloatingPointTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using EasySIMD.NET;
+
+namespace EasySIMD.NET.Test
+{
+    public class StatisticsExtensionsFloatingPointTests
+    {
+        private static int precision = 8;
+
+        // Sum
+
+        [Fact]
+        public void TestSumFloat()
+        {
+            var input = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+
+            var actual = input.Sum();
+            var expected = 55.0f;
+
+            Assert.Equal(expected, actual, precision);
+        }
+
+        [Fact]
+        public void TestSumDouble()
+        {
+            var input = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+
+            var actual = input.Sum();
+            var expected = 55.0;
+
+            Assert.Equal(expected, actual, precision);
+        }
+
+        // Product
+
+        [Fact]
+        public void TestProductFloat()
+        {
+            var input = new[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f };
+
+            var actual = input.Product();
+            var expected = 3628800.0f;
+
+            Assert.Equal(expected, actual, precision);
+        }
+
+        [Fact]
+        public void TestProductDouble()
+        {
+            var input = new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+
+            var actual = input.Product();
+            var expected = 3628800.0;
+
+            Assert.Equal(expected, actual, precision);
+        }
+    }
+}
--- EasySIMD.NET/StatisticsExtensions.cs
+++ EasySIMD.NET/StatisticsExtensions.cs
@@ -10,11 +10,15 @@ namespace EasySIMD.NET
         public static short Sum(this short[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
         public static int Sum(this int[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
         public static long Sum(this long[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
+        public static float Sum(this float[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
+        public static double Sum(this double[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
 
         // Product
 
         public static short Product(this short[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, (short)1);
         public static int Product(this int[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1);
         public static long Product(this long[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1L);
+        public static float Product(this float[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1.0f);
+        public static double Product(this double[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1.0);
     }
 }
