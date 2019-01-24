bc182586e41cc418c3e156090c5dbba73741e0d2
Robert Anderson <robertanderson1993@gmail.com>
2019-01-24 01:01:43 +0000

    Added other integer types for add method, added product method

--- EasySIMD.NET.Test/StatisticsExtensionsIntegerTests.cs
+++ EasySIMD.NET.Test/StatisticsExtensionsIntegerTests.cs
@@ -6,6 +6,8 @@ namespace EasySIMD.NET.Test
 {
     public class StatisticsExtensionsIntegerTests
     {
+        // Sum
+
         [Fact]
         public void TestSumShort()
         {
@@ -13,6 +15,63 @@ namespace EasySIMD.NET.Test
 
             var actual = input.Sum();
             var expected = 210;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestSumInt()
+        {
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var actual = input.Sum();
+            var expected = 55;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestSumLong()
+        {
+            var input = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
+
+            var actual = input.Sum();
+            var expected = 55L;
+
+            Assert.Equal(expected, actual);
+        }
+
+        // Product
+
+        [Fact]
+        public void TestProductShort()
+        {
+            var input = new short[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+
+            var actual = input.Product();
+            var expected = 1024;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestProductInt()
+        {
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var actual = input.Product();
+            var expected = 3628800;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestProductLong()
+        {
+            var input = new[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L };
+
+            var actual = input.Product();
+            var expected = 3628800L;
 
             Assert.Equal(expected, actual);
         }
--- EasySIMD.NET/StatisticsExtensions.cs
+++ EasySIMD.NET/StatisticsExtensions.cs
@@ -7,9 +7,14 @@ namespace EasySIMD.NET
     {
         // Sum
 
-        public static short Sum(this short[] input)
-        {
-            return input.Reduce(VectorOperators.Add, ScalarOperators.Add);
-        }
+        public static short Sum(this short[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
+        public static int Sum(this int[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
+        public static long Sum(this long[] input) => input.Reduce(VectorOperators.Add, ScalarOperators.Add);
+
+        // Product
+
+        public static short Product(this short[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, (short)1);
+        public static int Product(this int[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1);
+        public static long Product(this long[] input) => input.Reduce(VectorOperators.Multiply, ScalarOperators.Multiply, 1L);
     }
 }
